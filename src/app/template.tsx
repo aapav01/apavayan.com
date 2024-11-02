8a8b4632e72d0c0bb8bb7715e4b4cff43a197f0c
Apavayan Sinha <info@apavayan.com>
2024-11-02 23:19:50 +0530

    stagging: add Social Links

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from "react";
+import {
+  IoLogoFacebook,
+  IoLogoGithub,
+  IoLogoInstagram,
+  IoLogoLinkedin,
+} from "react-icons/io5";
 
 type Props = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
-export default function RootTemplate({
-    children,
-}: Props) {
+export default function RootTemplate({ children }: Props) {
   return (
-    <main className="container mx-auto py-10 md:p-10 lg:p-20">{children}</main>
-  )
+    <main className="container mx-auto py-10 md:p-10 lg:p-20">
+      <section className="flex flex-row gap-5 w-fit pb-4 ml-auto">
+        <a href="https://github.com/aapav01" target="_blank" className="text-3xl text-primary hover:text-zinc-700">
+          <IoLogoGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/apavayan/" target="_blank" className="text-3xl text-primary hover:text-blue-800">
+          <IoLogoLinkedin />
+        </a>
+        <a href="https://www.instagram.com/aapav/" target="_blank" className="text-3xl text-primary hover:text-pink-800">
+          <IoLogoInstagram />
+        </a>
+        <a href="https://www.facebook.com/aapav" target="_blank" className="text-3xl text-primary hover:text-blue-700">
+          <IoLogoFacebook  />
+        </a>
+      </section>
+      {children}
+    </main>
+  );
 }
