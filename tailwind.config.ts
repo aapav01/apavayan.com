eb5f954490cbf08d840eceeb392779b244d9969d
Apavayan Sinha <info@apavayan.com>
2025-04-24 01:21:52 +0530

    update: Shadcn/ui componenets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,6 +50,16 @@ const config: Config = {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
   		borderRadius: {
@@ -57,10 +67,14 @@ const config: Config = {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-		fontFamily: {
-			sans: ['var(--font-inter)'],
-			mono: ['var(--font-roboto-mono)'],
-		},
+  		fontFamily: {
+  			sans: [
+  				'var(--font-inter)'
+  			],
+  			mono: [
+  				'var(--font-roboto-mono)'
+  			]
+  		},
   		keyframes: {
   			'accordion-down': {
   				from: {
