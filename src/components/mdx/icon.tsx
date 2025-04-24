e5d65907a25858923963078a838469a5e10b1ad1
Apavayan Sinha <info@apavayan.com>
2025-04-24 09:13:07 +0530

    chore: clean up

--- src/components/mdx/icon.tsx
+++
@@ -1,8 +0,0 @@
-import SVG from "react-inlinesvg";
-
-// TODO: Add Interface for svg
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const Icon = ({ source, src, width = 28, ...props }: any) =>
-  source || src ? <SVG src={source || src} {...props} width={width} /> : null;
-
-export default Icon;
