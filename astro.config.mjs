9bbd40515b5646374731c0e3c9d7752291f46e80
Apavayan Sinha <info@apavayan.com>
2026-02-01 19:45:40 +0530

    add cloudflare app deployer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://apavayan.com',
+
   vite: {
     // @ts-expect-error - Vite version mismatch between Astro and Tailwind
     plugins: [tailwindcss()],
   },
+
   build: {
     inlineStylesheets: 'always', // Inline CSS for 14KB optimization
   },
-  compressHTML: true, // Minify HTML output
+
+  // Minify HTML output
+  compressHTML: true,
+
+  adapter: cloudflare()
 });
